19cd0be8b3b17ce21605b22eedde2d5bbda493a6
Becka7 <82467937+Becka7@users.noreply.github.com>
2022-09-27 09:39:28 +0300

    create the calculate function

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function ClearScreen(){
 function display(value){
     document.getElementById("result").value += value;
 }
+
+function calculate(){
+    var p = document.getElementById("result").value ;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+    
+}
