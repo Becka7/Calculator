b89ecd236883395880df5596f31f793dea8967b4
Becka7 <82467937+Becka7@users.noreply.github.com>
2022-09-27 09:55:18 +0300

    add comments

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function ClearScreen(){
     document.getElementById("result").value = "";
 }
+// the ClearScreen() function declares the value of result as an empty string 
 
 function display(value){
     document.getElementById("result").value += value;
 }
+// the display() function displays the value of the clicked buttons on the display-box
 
 function calculate(){
     var r = document.getElementById("result").value ;
@@ -12,3 +14,13 @@ function calculate(){
     document.getElementById("result").value = b;
     
 }
+/*
+() eval() function evaluates javascript code and executes it
+--------------------------------------------------------------
+ testing the eval() function 
+ -------------------------------------------------------------
+ var b = 49/7;
+ var c = eval(b);
+ console.log(c);
+ -------------------------------------------------------------
+ **/
